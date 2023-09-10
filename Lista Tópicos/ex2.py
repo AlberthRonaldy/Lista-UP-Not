3aacae019e5f8c87a0247cc55f33fd721c0016bb
alberth <alberthronaldy@gmail.com>
2023-09-10 20:14:48 -0300

    ex2

---
+++ Lista Tópicos/ex2.py
@@ -0,0 +1,65 @@
+class JogoDaVelhaNxN:
+    def __init__(self, tamanho):
+        self.tamanho = tamanho
+        self.tabuleiro = [[' ' for _ in range(tamanho)] for _ in range(tamanho)]
+        self.jogador_atual = 'X'
+        self.jogadas = 0
+
+    def imprimir_tabuleiro(self):
+        for linha in self.tabuleiro:
+            print('|'.join(linha))
+            print('-' * (self.tamanho * 2 - 1))
+
+    def verificar_vitoria(self):
+        # Verificação de linhas e colunas
+        if any(all(self.tabuleiro[i][j] == self.jogador_atual for i in range(self.tamanho)) for j in range(self.tamanho)):
+            return True
+
+        # Verificação de colunas usando all
+        if any(all(self.tabuleiro[i][j] == self.jogador_atual for j in range(self.tamanho)) for i in range(self.tamanho)):
+            return True
+
+        # Verificação de diagonais usando all
+        if all(self.tabuleiro[i][i] == self.jogador_atual for i in range(self.tamanho)) or \
+           all(self.tabuleiro[i][self.tamanho - 1 - i] == self.jogador_atual for i in range(self.tamanho)):
+            return True
+
+        return False
+
+    def jogar(self):
+        while True:
+            self.imprimir_tabuleiro()
+            print(f'Jogador {self.jogador_atual}, é sua vez.')
+
+            try:
+                linha = int(input(f'Informe o número da linha (0 a {self.tamanho - 1}): '))
+                coluna = int(input(f'Informe o número da coluna (0 a {self.tamanho - 1}): '))
+            except ValueError:
+                print('Entrada inválida. Tente novamente.')
+                continue
+
+            # Verifica se a jogada é válida
+            if linha < 0 or linha >= self.tamanho or coluna < 0 or coluna >= self.tamanho or self.tabuleiro[linha][coluna] != ' ':
+                print('Jogada inválida. Tente novamente.')
+                continue
+
+            self.tabuleiro[linha][coluna] = self.jogador_atual
+            self.jogadas += 1
+
+            if self.verificar_vitoria():
+                self.imprimir_tabuleiro()
+                print(f'Jogador {self.jogador_atual} venceu!')
+                break
+
+            if self.jogadas == self.tamanho * self.tamanho:
+                self.imprimir_tabuleiro()
+                print('O jogo empatou!')
+                break
+
+            # Troca o jogador
+            self.jogador_atual = 'X' if self.jogador_atual == 'O' else 'O'
+
+if __name__ == "__main__":
+    tamanho_tabuleiro = int(input('Informe o tamanho do tabuleiro (NxN): '))
+    jogo = JogoDaVelhaNxN(tamanho_tabuleiro)
+    jogo.jogar()
